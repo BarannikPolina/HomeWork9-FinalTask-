09ffd2fc759f391e0dde74da8d60ead220ac91d9
Barannik Polina <barannik.polina@mail.ru>
2022-11-23 18:53:27 +0300

    Задача итогового проекта

---
+++ HomeWork9 (Final Task)/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] arr = {"555", "*", "0", "%", "39", "781", "#", "&", ":-)", "87", "9", "129", "2", "@"};
+
+for (int i = 0; i < arr.Length; i++) 
+{
+Boolean c = (Char.IsDigit(arr[i], 0));
+if (!c) 
+{
+Console.WriteLine(arr[i]);
+}
+}
+
